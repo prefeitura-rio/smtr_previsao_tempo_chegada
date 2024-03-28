e9c38a50be0f6a7b84b55262b73c16bc828cceca
Igor Rigolon <igor.rilave@hotmail.com>
2024-03-28 16:44:32 -0300

    escrevendo a ideia de como montar

--- puc/base_validacao.R
+++ puc/base_validacao.R
@@ -8,4 +8,10 @@ gtfs <- readr::read_rds("data/gtfs.rds")
 
 gps <- readr::read_csv(file.path(source, "gps_sample.csv"))
 
-# projetando a posição
+# projetando a posição de gps na shapefile:
+# obter a distancia total que o ônibus já percorreu
+
+# por essa distância, detectar a parada anterior e a próxima do ônibus
+
+# medir em quanto tempo o ônibus chegou na próxima parada
+# (quando a próxima parada se tornou parada anterior)
